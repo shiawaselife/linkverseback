d0451413e6be981d33ea17981b10b553abb07aa6
김규빈 <daegukgb@kakao.com>
2025-05-11 23:27:58 +0900

    implement basic functions

---
+++ src/main/java/site/linkverse/back/testtest/af.java
@@ -0,0 +1,7 @@
+package site.linkverse.back.testtest;
+
+public class af {
+    public static void main(String[] args) {
+        asdf as2df = new asdf();
+    }
+}
---
+++ src/main/java/site/linkverse/back/testtest/asdf.java
@@ -0,0 +1,11 @@
+package site.linkverse.back.testtest;
+
+public class asdf {
+    private int asfd;
+    private String sdf;
+
+    public asdf(int asfd, String sdf) {
+        this.asfd = asfd;
+        this.sdf = sdf;
+    }
+}
